b5bd818c94479a7e9f198471d8456475c40386e0
baokui <sy880115>
2020-01-19 19:57:16 +0800

    get data

--- myscript/run_getInputData.sh
+++ myscript/run_getInputData.sh
@@ -1,3 +1,4 @@
 alias mhadoop='/search/odin/software/MarsJs/bin/hadoop'
 hadoopclients=ml_research,3evrqV2R
-mhadoop fs -Dhadoop.client.ugi=$hadoopclients -get VpaOutput_guobk/logdata-process/UserInputAll_seg/* ../../data/data_inputs/
+mkdir log
+mhadoop fs -Dhadoop.client.ugi=$hadoopclients -get VpaOutput_guobk/logdata-process/UserInputAll_seg/* ../../data/data_inputs/ >> ./log/log.getdata 2>&1 &
