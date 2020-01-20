211b83e65c689517d8d22549c25c36123880974f
baokui <sy880115>
2020-01-20 10:44:25 +0800

    data segment

--- myscript/textseg.py
+++ myscript/textseg.py
@@ -4,9 +4,9 @@ import jieba_fast as jieba
 import os
 import sys
 def textSeg(sourefile0,targetfile0):
+    targetfile = targetfile0 + '/sents.txt'
     for p in range(5):
         sourefile = sourefile0+'/part-0000'+str(p)
-        targetfile = targetfile0+'/part-0000'+str(p)
         f_r = open(sourefile,'r')
         f_w = open(targetfile,'w+')
         for line in f_r:
@@ -15,10 +15,10 @@ def textSeg(sourefile0,targetfile0):
             if len(s)!=3:
                 continue
             words = " ".join(jieba.lcut(s[2], HMM=True))
-            f_w.write(words.encode('utf-8')+'\n')
+            f_w.write(words+'\n')
         f_w.close()
         f_r.close()
-        os.remove(sourefile)
+        #os.remove(sourefile)
 def main(sourefile):
     os.mkdir(sourefile+'-seg')
     textSeg(sourefile,sourefile+'-seg')
