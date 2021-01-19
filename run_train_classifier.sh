e8945b0d5f7b6fd13263fd769a0083b6a27b49ee
apple-guobk <baokui2011@163.COM>
2021-01-19 19:51:48 +0800

    bert_vpa_label_server

--- run_train_classifier.sh
+++ run_train_classifier.sh
@@ -12,11 +12,12 @@ nohup python -u run_classifier.py \
     --train_batch_size=8 \
     --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
     --max_seq_length=512 \
-    --do_train=True \
-    --do_eval=True >> log/labelmodel-$task_name.log 2>&1 &
+    --do_predict=True \
+    --do_train=False \
+    --do_eval=False >> log/labelmodel-test-$task_name.log 2>&1 &
 export CUDA_VISIBLE_DEVICES=5
 BERT_BASE_DIR=/search/odin/guobk/vpa/roberta_zh/model/roberta_zh_l12
-task_name=表达对象p0
+task_name=表达对象P0
 mkdir -p model/label/$task_name
 mkdir log
 nohup python -u run_classifier.py \
@@ -28,12 +29,12 @@ nohup python -u run_classifier.py \
     --train_batch_size=8 \
     --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
     --max_seq_length=512 \
-    --do_train=True \
-    --do_eval=True >> log/labelmodel-$task_name.log 2>&1 &
-
+    --do_predict=True \
+    --do_train=False \
+    --do_eval=False >> log/labelmodel-test-$task_name.log 2>&1 &
 export CUDA_VISIBLE_DEVICES=6
 BERT_BASE_DIR=/search/odin/guobk/vpa/roberta_zh/model/roberta_zh_l12
-task_name=表达者性别倾向p0
+task_name=表达者性别倾向P0
 mkdir -p model/label/$task_name
 mkdir log
 nohup python -u run_classifier.py \
@@ -45,9 +46,9 @@ nohup python -u run_classifier.py \
     --train_batch_size=8 \
     --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
     --max_seq_length=512 \
-    --do_train=True \
-    --do_eval=True >> log/labelmodel-$task_name.log 2>&1 &
-
+    --do_predict=True \
+    --do_train=False \
+    --do_eval=False >> log/labelmodel-test-$task_name.log 2>&1 &
 #export CUDA_VISIBLE_DEVICES=4
 #BERT_BASE_DIR=/search/odin/guobk/vpa/roberta_zh/model/roberta_zh_l12
 #task_name=文字风格
