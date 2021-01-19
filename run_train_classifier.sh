e419c969c828ac284b59ad6eb68edef238a071d2
apple-guobk <baokui2011@163.COM>
2021-01-19 15:03:33 +0800

    train_vpa_label

--- run_train_classifier.sh
+++ run_train_classifier.sh
@@ -9,7 +9,7 @@ nohup python -u run_classifier.py \
     --task_name=$task_name \
     --vocab_file=$BERT_BASE_DIR/vocab.txt \
     --output_dir=model/label/$task_name \
-    --train_batch_size=32 \
+    --train_batch_size=2 \
     --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
     --max_seq_length=512 \
     --do_train=True \
