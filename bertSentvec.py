00b80c230584bdb5078b8e2f9e9da7fa323fa5c3
apple-guobk <baokui2011@163.COM>
2021-02-03 17:44:50 +0800

    bert 有监督语义向量训练

--- bertSentvec.py
+++ bertSentvec.py
@@ -626,7 +626,7 @@ def create_model(bert_config, is_training, input_ids, input_mask, segment_ids,
     print("TEST:")
     score = cosine(feature_qr,feature_dc)
     print(score,labels)
-    c = tf.square(score - tf.to_float32(labels))
+    c = tf.square(score - tf.cast(labels, dtype=tf.float32))
     per_example_loss = tf.reduce_mean(c,axis=-1)
     loss = tf.reduce_mean(per_example_loss)
     return (loss,per_example_loss,feature_qr,feature_dc)
