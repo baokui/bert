6b8cec15feeaa0a0d4394baf8c31426b4818c6a2
apple-guobk <baokui2011@163.COM>
2021-02-03 16:55:11 +0800

    bert 有监督语义向量训练

--- bertSentvec.py
+++ bertSentvec.py
@@ -521,8 +521,8 @@ def file_based_convert_examples_to_features(
             f = tf.train.Feature(int64_list=tf.train.Int64List(value=list(values)))
             return f
         features = collections.OrderedDict()
-        features["input_ids"] = create_int_feature(feature.input_ids)
-        features["input_mask"] = create_int_feature(feature.input_mask)
+        features["input_ids"] = [create_int_feature(feature.input_ids[0]),create_int_feature(feature.input_ids[1])]
+        features["input_mask"] = [create_int_feature(feature.input_mask[0]),create_int_feature(feature.input_mask[1])]
         features["segment_ids"] = create_int_feature(feature.segment_ids)
         features["label_ids"] = create_int_feature([feature.label_ids])
         features["is_real_example"] = create_int_feature(
